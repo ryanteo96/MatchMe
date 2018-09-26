acf9481ad7822255d182c8839ee3db14c5de83b9
Jay Patel <jayptl96@gmail.com>
2018-09-26 14:52:58 -0400

    Simplified login

--- app.js
+++ app.js
@@ -7,51 +7,83 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/User');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://matchers:matchme1!@ds113703.mlab.com:13703/match-me')
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var cors = require('cors')
 var app = express();
 
-app.use(cors())
+mongoose.connect('mongodb://matchers:matchme1!@ds113703.mlab.com:13703/match-me')
+    .then(() => console.log('connection succesful'))
+    .catch((err) => console.error(err));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(require("express-session")({
+    secret: "matchme is awesome",
     resave: false,
     saveUninitialized: false
 }));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// passport configuration
-// file user.js has the configuration for login and register
-var User = require('./models/User');
-
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", function (req, res) {
+    res.render("login");
+});
+
+app.get("/index", isLoggedIn, function (req, res) {
+    res.render("index");
+});
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    User.register(new User({
+        username: req.body.username,
+        name: req.body.name
+    }), req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/index");
+        });
+    });
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+})
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/index",
+    failureRedirect: "/login"
+}), function (req, res) {
+    res.send("User is " + req.user.id);
+});
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect('/');
+
+})
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = app;
